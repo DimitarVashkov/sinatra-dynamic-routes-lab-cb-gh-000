d0a78f99f5767b3b067f59705cc007b94531d6e7
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-02 16:39:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,6 +28,14 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
+  get '/:operation/:number1/:number2' do
+    @op = params[:operation]
+    @n1 = params[:number1]
+    @n2 = params[:number2]
+
+    if @op == '+'
+  end
+
 
 
 end
